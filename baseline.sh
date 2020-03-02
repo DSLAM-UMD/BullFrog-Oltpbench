b564ae0df217c8b27d2b939c8a17d95ceef3c8f9
root <root@abadi-computer.cs.umd.edu>
2020-03-02 07:26:30 -0500

    change base config

--- baseline.sh
+++ baseline.sh
@@ -10,5 +10,5 @@ cd ~/lazyschema/oltpbench/
 
 ./oltpbenchmark -b tpcc -c config/pgtpcc_base_config.xml --create=true --load=true --port=5435 --path=/home/gangliao/lazyschema/postgres/dev/bin
 
-./oltpbenchmark -b tpcc -c config/pgtpcc_base_config.xml  --execute=true -s 5 -o base_join  --port=5435 --path=/home/gangliao/lazyschema/postgres/dev/bin
+./oltpbenchmark -b tpcc -c config/pgtpcc_base_config.xml  --execute=true -s 5 -o base_join  --port=5435 --path=/home/gangliao/lazyschema/postgres/dev/bin --migration=6
 
