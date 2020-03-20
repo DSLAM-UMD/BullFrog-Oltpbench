ef11da7994ab720474f02d36c784ebd3d8534817
Gang Liao <gangliao@cs.umd.edu>
2020-03-20 10:48:27 +0800

    revise configs

--- tools/plot/plot.sh
+++ tools/plot/plot.sh
@@ -6,5 +6,5 @@ set output "$1.png"
 set xlabel "Time [Seconds]"
 set ylabel "Throughput [TPs]"
 set title "TPC-C Benchmark - rate: 300, terminal: 5"
-plot [] [0:350] "$1" using 1:2 t "Migration Join - Lazy" w l
+plot [] [0:350] "$1" using 1:2 t "Migration Projection - Lazy" w l
 EOF
