56fbd242170784115640d6e7247b8c9910123ff1
Gang Liao <gangliao@cs.umd.edu>
2020-03-20 15:18:27 +0800

    update scale factor for base proj

--- scripts/base_proj.sh
+++ scripts/base_proj.sh
@@ -10,5 +10,5 @@ cd ~/lazyschema/oltpbench/
 
 ./oltpbenchmark -b tpcc -c config/pgtpcc_base_proj.xml --create=true --load=true --port=5434 --path=/home/gangliao/lazyschema/postgres/dev/bin
 
-./oltpbenchmark -b tpcc -c config/pgtpcc_base_proj.xml  --execute=true -s 5 -o base_join  --port=5434 --path=/home/gangliao/lazyschema/postgres/dev/bin --migration=6
+./oltpbenchmark -b tpcc -c config/pgtpcc_base_proj.xml  --execute=true -s 5 -o base_proj  --port=5434 --path=/home/gangliao/lazyschema/postgres/dev/bin --migration=6
 
